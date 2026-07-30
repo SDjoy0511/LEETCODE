4514cc846b04793cb89c4c4510713cc8ed766631
Srinjoy Das <dassrinjoy007@gmail.com>
2026-07-31 01:14:13 +0530

    Time: 2 ms (58.32%) | Memory: 12.2 MB (12.00%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+bool fun(unordered_map<char,int>have,
+        unordered_map<char,int>need){
+            for(auto i : need){
+                char c = i.first;
+                int fneed= i.second;
+                int fhave= have[c];
+                if(fhave<fneed)
+                    return false;
+            }
+            return true;
+        }
+    bool canConstruct(string ransomNote, string magazine) {
+         unordered_map<char,int>need;
+        unordered_map<char,int>have;
+        int i;
+        for(i=0;i<ransomNote.size();i++){
+            need[ransomNote[i]]++;
+        }
+        for(i=0;i<magazine.size();i++){
+            have[magazine[i]]++;
+        }
+        return fun(have,need);
+    }
+};
